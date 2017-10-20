afba2b3f1df8d3b9df34528fe2e764582b88bac7
Luiz Alb. Silva <luizalbsilva@gmail.com>
2017-10-20 18:35:56 -0200

    Carregando... (cidade)

--- wp-content/themes/integral/header.php
+++ wp-content/themes/integral/header.php
@@ -23,6 +23,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="stylesheet" href="<?= get_template_directory_uri()?>/css/jquery-ui.min.css">
+<link rel="stylesheet" href="<?= get_template_directory_uri()?>/css/candidatou.css">
 <?php wp_head(); ?>
 <script type="text/javascript" src="<?= get_template_directory_uri()?>/js/jquery-ui.min.js"></script>
 </head>

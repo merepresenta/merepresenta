4aa927b9e0d4b0dee7c3177b2b918ba18ed5ee62
Luiz Alb. Silva <luizalbsilva@gmail.com>
2017-12-13 22:01:53 -0200

    Alterando os textos das pautas

--- interno/sqls/atualiza-texto-perguntas.sql
+++ interno/sqls/atualiza-texto-perguntas.sql
@@ -1,83 +1,83 @@
 update Pergunta 
   set 
-    texto = 'Você é a favor da adoção de crianças por famílias LGBTs'
+    texto = 'Você é a favor da adoção de crianças por famílias LGBTs?'
     , texto_pesquisa = 'Adoção por famílias LGBTs'
   where id = 1;
 
 update Pergunta 
   set 
-    texto = 'Você é a favor do uso de banheiros por pessoas travestis e transexuais de acordo com sua identidade de gênero'
+    texto = 'Você é a favor do uso de banheiros por pessoas travestis e transexuais de acordo com sua identidade de gênero?'
     , texto_pesquisa = 'Respeito à identidade de gênero de pessoas trans'
     where id = 2;
 
 update Pergunta 
   set 
-    texto = 'Você é a favor da cota de 50% para mulheres no Legislativo, garantindo representatividade étnico-racial e respeito à identidade de gênero autodeclarada'
+    texto = 'Você é a favor da cota de 50% para mulheres no Legislativo, garantindo representatividade étnico-racial e respeito à identidade de gênero autodeclarada?'
     , texto_pesquisa = 'Cota para mulheres no legislativo'
     where id = 3;
 
 update Pergunta 
   set 
-    texto = 'Você é a favor da promoção de igualdade de gênero e raça e o respeito às orientações sexuais e identidades de gênero nas escolas'
+    texto = 'Você é a favor da promoção de igualdade de gênero e raça e o respeito às orientações sexuais e identidades de gênero nas escolas?'
     , texto_pesquisa = 'Igualdade de gênero e raça nas escolas'
     where id = 4;
 
 update Pergunta 
   set 
-    texto = 'Você é a favor das cotas raciais e ações afirmativas para a população negra'
+    texto = 'Você é a favor das cotas raciais e ações afirmativas para a população negra?'
     , texto_pesquisa = 'Ações afirmativas raciais'
     where id = 5;
 
 update Pergunta 
   set 
-    texto = 'Você é a favor da proibição da realização de cultos e o uso de símbolos religiosos em repartições públicas'
+    texto = 'Você é a favor da proibição da realização de cultos e o uso de símbolos religiosos em repartições públicas?'
     , texto_pesquisa = 'Estado Laico'
     where id = 6;
 
 update Pergunta 
   set 
-    texto = 'Você é a favor do impedimento de ocupação de cargos públicos por homens que tenham agredido mulheres'
+    texto = 'Você é a favor do impedimento de ocupação de cargos públicos por homens que tenham agredido mulheres?'
     , texto_pesquisa = 'Combate à violência contra mulher'
     where id = 7;
 
 update Pergunta 
   set 
-    texto = 'Você é a favor da descriminalização e legalização do aborto'
+    texto = 'Você é a favor da descriminalização e legalização do aborto?'
     , texto_pesquisa = 'Descriminalização do aborto'
     where id = 8;
 
 update Pergunta 
   set 
-    texto = 'Você é a favor da criminalização da lesbofobia, homofobia, transfobia e bifobia'
+    texto = 'Você é a favor da criminalização da lesbofobia, homofobia, transfobia e bifobia?'
     , texto_pesquisa = 'Criminalização da LGBTfobia'
     where id = 9;
 
 update Pergunta 
   set 
-    texto = 'Você é a favor da desmilitarização da polícia'
+    texto = 'Você é a favor da desmilitarização da polícia?'
     , texto_pesquisa = 'Desmilitarização da polícia'
     where id = 10;
 
 update Pergunta 
   set 
-    texto = 'Você é a favor da desapropriação de imóveis abandonados para criação de moradias de interesse social'
+    texto = 'Você é a favor da desapropriação de imóveis abandonados para criação de moradias de interesse social?'
     , texto_pesquisa = 'Desapropriação por interesse social'
     where id = 11;
 
 update Pergunta 
   set 
-    texto = 'Você é a favor do licenciamento ambiental que proteja populações vulneráveis e áreas atingidas por grandes empreendimentos'
+    texto = 'Você é a favor do licenciamento ambiental que proteja populações vulneráveis e áreas atingidas por grandes empreendimentos?'
     , texto_pesquisa = 'Defesa do meio ambiente'
     where id = 12;
 
 update Pergunta 
   set 
-    texto = 'Você é a favor da abertura de maiores espaços de participação direta da população na definição do orçamento municipal'
+    texto = 'Você é a favor da abertura de maiores espaços de participação direta da população na definição do orçamento municipal?'
     , texto_pesquisa = 'Orçamento participativo'
     where id = 13;
 
 update Pergunta 
   set 
-    texto = 'Você é a favor da quebra dos contratos com empresas de mobilidade que não têm auditoria de custos e uma gestão transparente'
+    texto = 'Você é a favor da quebra dos contratos com empresas de mobilidade que não têm auditoria de custos e uma gestão transparente?'
     , texto_pesquisa = 'Transparência'
     where id = 14;

9f61c38ac57471441bfdd68e7fd702c6d930b671
Luiz Alb. Silva <luizalbsilva@gmail.com>
2017-12-14 23:50:12 -0200

    Clipping - Parando carrosel e criando outro para materias

--- wp-content/themes/integral/page-clipping.php
+++ wp-content/themes/integral/page-clipping.php
@@ -14,101 +14,124 @@
 <?php
   get_header();
 ?>
-<h2 class="entry-title"><?php the_title(); ?></h2>
-      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-<p><?php the_content(); ?></p>
-<?php endwhile;?>
-<?php endif; ?>
+<div class="container">
+  
+
+  <h2 class="entry-title"><?php the_title(); ?></h2>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+  <p><?php the_content(); ?></p>
+  <?php endwhile;?>
+  <?php endif; ?>
 
 
-<div class="container" id="page-clipping">
-  <div class="row">
+  <div id="carousel-clipping" class="carousel" data-ride="carousel" data-interval="false">
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
     <?php
     $cnt = 0;
-    query_posts( 'cat=4&posts_per_page=4' );// for articles from newspapers or related
+    query_posts( 'cat=4' );// for videos
     while ( have_posts() ) : the_post();
+      $url = get_post_meta( get_the_ID(), 'url' );
+      if($url && sizeof($url) > 0) $url = $url[0];
+      else $url = null;
+      if (($cnt % 4) == 0) :
     ?>
-      <div class="col-md-3">
-        <div class="panel panel-default">
-          <div class="panel-body">
+      <div class="row item <?php if ($cnt == 0) { echo "active"; } ?>">
+    <?php endif ?>
+        <div class="col-sm-6 col-md-3">
             <span data-toggle="modal" data-target="#myModal-<?=$cnt?>"><?=the_post_thumbnail(array( 200, 300 ));?></span>
-            <h3><?=the_title();?></h3>
-            <h4><?php the_time('d/m/Y'); ?></h4>
-
+            <h4><?= $url == null ? the_title() : ("<a href='$url'>" . the_title() . '</a>') ?></h4>
+            <h5><?php the_time('d/m/Y'); ?></h5>
             <div class="modal fade" id="myModal-<?=$cnt?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
               <div class="modal-dialog" role="document">
                 <?=the_post_thumbnail( 'full' );?>
               </div>
             </div>
+        </div>
+    <?php if (($cnt % 4) == 3) : ?>
+    </div>
+    <?php endif ?>
+      
+    <?php
+      $cnt++;
+    endwhile;
+    wp_reset_query();
+    if ((($cnt-1) % 4) != 3) : ?>
+      </div>
+    <?php endif ?>
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#carousel-clipping" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#carousel-clipping" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+  <div class="container carousel slide" id="page-video" data-ride="carousel"  data-interval="false">
+    <!-- destacado -->
+    <div class="row carousel-inner" role="listbox">
+      <?php
+      $cnt = 0;
+      query_posts( 'cat=3' );// for videos
+      while ( have_posts() ) : the_post();
+        $url_video = get_post_meta( get_the_ID(), 'video' );
+        $embed_video = substr($url_video[0],-11);
+      ?>
+      <div class="item <?=($cnt==0)?'active':''?>">
+        <div class="col-md-8">
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" width="560" height="315" src="//www.youtube-nocookie.com/embed/<?=$embed_video?>" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <h4><?=the_title();?></h4>
+          <h5><?php the_time('d/m/Y'); ?></h5>
+          <?=the_content()?>
+        </div>
+      </div>
+      <?php
+        $cnt++;
+      endwhile;
+      wp_reset_query();
+      ?>
+    </div>
+    <!-- destacado -->
+
+    <div class="row" class="carousel-indicators">
+      <?php
+      $cnt = 0;
+      query_posts( 'cat=3' );// for videos
+      while ( have_posts() ) : the_post();
+      ?>
+      <div class="col-md-2" data-target="#page-video" data-slide-to="<?=$cnt?>">
+        <div class="panel panel-default">
+          <div class="panel-body">
+            <?php
+            $url_video = get_post_meta( get_the_ID(), 'video' );
+            $preview_video = substr($url_video[0],-11);
+            ?>
+            <img src="//img.youtube.com/vi/<?=$preview_video?>/default.jpg" alt="<?=the_title();?>">
+            <h4><?=the_title();?></h4>
+            <h5><?php the_time('d/m/Y'); ?></h5>
           </div>
         </div>
       </div>
-    <?php
-      $cnt++;
-    endwhile;
-    wp_reset_query();
-    ?>
+      <?php
+        $cnt++;
+      endwhile;
+      wp_reset_query();
+      ?>
+    </div><!-- .row -->
   </div>
 </div>
 
-
-<div class="container carousel slide" id="page-video" data-ride="carousel">
-  <!-- destacado -->
-  <div class="row carousel-inner" role="listbox">
-    <?php
-    $cnt = 0;
-    query_posts( 'cat=3' );// for videos
-    while ( have_posts() ) : the_post();
-      $url_video = get_post_meta( get_the_ID(), 'video' );
-      $embed_video = substr($url_video[0],-11);
-    ?>
-    <div class="item <?=($cnt==0)?'active':''?>">
-      <div class="col-md-8">
-        <div class="embed-responsive embed-responsive-16by9">
-          <iframe class="embed-responsive-item" width="560" height="315" src="//www.youtube-nocookie.com/embed/<?=$embed_video?>" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
-        </div>
-      </div>
-      <div class="col-md-4">
-        <h4><?=the_title();?></h4>
-        <h5><?php the_time('d/m/Y'); ?></h5>
-        <?=the_content()?>
-      </div>
-    </div>
-    <?php
-      $cnt++;
-    endwhile;
-    wp_reset_query();
-    ?>
-  </div>
-  <!-- destacado -->
-
-  <div class="row" class="carousel-indicators">
-    <?php
-    $cnt = 0;
-    query_posts( 'cat=3' );// for videos
-    while ( have_posts() ) : the_post();
-    ?>
-    <div class="col-md-2" data-target="#page-video" data-slide-to="<?=$cnt?>">
-      <div class="panel panel-default">
-        <div class="panel-body">
-          <?php
-          $url_video = get_post_meta( get_the_ID(), 'video' );
-          $preview_video = substr($url_video[0],-11);
-          ?>
-          <img src="//img.youtube.com/vi/<?=$preview_video?>/default.jpg" alt="<?=the_title();?>">
-          <h4><?=the_title();?></h4>
-          <h5><?php the_time('d/m/Y'); ?></h5>
-        </div>
-      </div>
-    </div>
-    <?php
-      $cnt++;
-    endwhile;
-    wp_reset_query();
-    ?>
-  </div><!-- .row -->
-</div>
 <?php
   get_footer();
 ?>
